acc112a23135934e5bee8c1cfa6f3f6b3809dfc5
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-11-20 17:20:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+    // f(n) = f (n-2) + f (n-1)
+    // f(1) = 1
+    // f(2) = 1
+    // f(3) = 2
+    static int f(int n) {
+
+        if (n == 1 || n == 2)
+            return 1;
+        else
+            return f(n - 1) + f(n - 2);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(f(3));
+
     }
 }
